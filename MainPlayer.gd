880d3233099c5a59e72f407fda31fb7079362e18
Bernardo Amaral <bernardo2018amaral@gmail.com>
2025-09-01 21:24:29 -0300

    Add files via upload

---
+++ MainPlayer.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var speed = 300
+
+func _process(delta: float) -> void:
+	playerMovementKeyboard(delta)
+	
+
+func playerMovementKeyboard(delta):
+	var movement = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		movement.x += 1
+	if Input.is_action_pressed("ui_left"):
+		movement.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		movement.y += 1
+	if Input.is_action_pressed("ui_up"):
+		movement.y -= 1
+	movement = movement.normalized()
+	position += movement * speed * delta
+	
+
+	
