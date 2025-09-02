09d9fd9b19ac336d4bd5988311c9ed11d34e801a
Bernardo Amaral <bernardo2018amaral@gmail.com>
2025-09-02 20:25:09 -0300

    Add files via upload

---
+++ spawner.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var shark = preload("res://node_2d.tscn")
+@onready var spawn_points = $SpawnPoints.get_children()
+@onready var spawn_parent = get_node("../Enemies")
+
+func _on_timer_timeout() -> void:
+	var tubarao = shark.instantiate()
+	var spawn = spawn_points[randi() % spawn_points.size()]
+	tubarao.global_position = spawn.global_position
+	spawn_parent.add_child(tubarao)
