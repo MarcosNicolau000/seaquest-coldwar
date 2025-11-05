59b9a983f6c987452b44d8a51f44bce2ff4a4397
MarcosNicolau000 <marcosnicolauoliveira@outlook.com>
2025-11-04 21:14:47 -0300

    docs: add comments to GameStartRoutine

--- JogoPrincipal/GameStart/GameStartRoutine.gd
+++ JogoPrincipal/GameStart/GameStartRoutine.gd
@@ -1,37 +1,42 @@
 extends Node
 
+const OXYGEN_AREA = -160
+const SCORE_RATE = 5  
+const OXYGEN_INCREASE_RATE = 3
+
 var is_game_running = false
 var elapsed_time = 0.0
 var scoreCount = 0
-const SCORE_RATE = 5  # Pontos por segundo
 var game_life = 3
 var gamePhase = 0
 var oxygenCount = 1000
-const OXYGEN_AREA = -160
-const OXYGEN_INCREASE_RATE = 3
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	start_game()
-	pass # Replace with function body.
+	pass
 
 
 
 func start_game():
+	
+	# VARIÁVEIS DE INÍCIO DE JOGO
 	gamePhase = 1
 	elapsed_time = 0 
 	scoreCount = 0
 	oxygenCount = 100000
 
 func _process(delta: float) -> void:
-	elapsed_time += delta
+	# CONTADOR DE TEMPO
+	elapsed_time += delta	
 	
-	
+	# SISTEMA DE OXIGÊNIO
 	if ($"../Player".position[1] < OXYGEN_AREA):
 		if (oxygenCount < 100000):
 			oxygenCount += elapsed_time*OXYGEN_INCREASE_RATE
 	else:
 		oxygenCount -= elapsed_time
+
 	
-	print(oxygenCount)
+	# CONTADOR DE SCORE
 	scoreCount = int(elapsed_time * SCORE_RATE)
 	pass
