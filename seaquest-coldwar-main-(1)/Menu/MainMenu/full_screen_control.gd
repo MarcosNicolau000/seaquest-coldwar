ba50092f9015bc34b3dfc549aec708cce7f29ae4
Marcos Nicolau <69830188+MarcosNicolau000@users.noreply.github.com>
2025-11-17 08:27:18 -0300

    Revert "New enemies added"

--- seaquest-coldwar-main-(1)/Menu/MainMenu/full_screen_control.gd
+++
@@ -1,9 +0,0 @@
-extends CheckButton
-
-
-func _on_toggled(toggled_on: bool):
-	if toggled_on == true:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-	
